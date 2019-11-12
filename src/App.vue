71d999bcf99d7d3bb94c639876c8312590433e10
yeweimao <1097564183@qq.com>
2019-11-12 21:27:06 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <component :is="currentComponet"></component>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+    import ActionOne from './components/action-one'
+    import ActionTwo from './components/action-two'
+
+    export default {
+        name: 'App',
+        components: {
+            ActionOne,
+            ActionTwo
+        },
+        data() {
+            return {
+                ActionOne: ActionOne,
+                ActionTwo: ActionTwo,
+                index: 1
+            }
+        },
+        computed: {
+            currentComponet() {
+                if (this.index == 1) {
+                    return this.ActionTwo
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
